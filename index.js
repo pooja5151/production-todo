4dd3f4292c16ac8fbba32751b6ab3954eb99f372
TUMCHA_NAAV <TUMCHA_EMAIL>
2024-02-09 19:01:21 +0530

    ui added

--- index.js
+++ index.js
@@ -2,13 +2,19 @@ const express = require("express")
 const cors = require("cors")
 require("dotenv").config({ path: "./.env" })
 const mongoose = require("mongoose")
+const path = require("path")
 
 mongoose.connect(process.env.MONGO_URL)
 const app = express()
 app.use(express.json())
 app.use(cors())
 
+app.use(express.static(path.join(__dirname, "dist")))
+
 app.use("/api/admin", require("./routes/todoRoute"))
+app.use("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "dist", "index.html"))
+})
 
 
 mongoose.connection.once("open", () => {
